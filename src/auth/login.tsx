0126cfdedc9f8ac062372d49adc7ee0491a04f90
mdarf <mdarfinji45@gmail.com>
2024-11-15 05:00:35 +0800

    addinf label

--- src/auth/login.tsx
+++ src/auth/login.tsx
@@ -1,14 +1,22 @@
-
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 const Login = () => {
   return (
-    <div className="flex item-start justify-center min-h-screen ">
-        <form className=" md:p-8 w-full max-w-md md:border  border-gray-200 rounded-lg mx-4">
-            <h1>HELLO WORLD </h1>
-
-        </form>
+    <div className="flex items-center justify-center min-h-screen ">
+      <form className="md:p-8 w-full max-w-md md:border  border-gray-200 rounded-lg mx-4">
+        <div className="mb-4">
+          <h1>Food Hub</h1>
+        </div>
+        <div>
+          <Label>Email</Label>
+          <Input type="email" placeholder="Enter Your Email " />
+          <Label>Password</Label>
+          <Input type="Password" placeholder="Enter Your Password " />
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
