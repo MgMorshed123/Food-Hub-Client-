8e13cc3790dc52807d3800db714e1bf1a8e090c0
mdarf <mdarfinji45@gmail.com>
2024-11-18 04:32:24 +0800

    addding searh functionaly

--- src/components/SearchPage.tsx
+++ src/components/SearchPage.tsx
@@ -1,14 +1,33 @@
 import { useParams } from "react-router-dom";
 import FilterPage from "./FilterPage";
+import { Input } from "./ui/input";
+import { useState } from "react";
+import { Button } from "./ui/button";
 
 const SearchPage = () => {
   const params = useParams();
+  const [searchQuery, setSearchQuery] = useState<string>("");
 
   return (
     <div className="max-w-7xl mx-auto my-10">
-      <div className="flex flex-col d:flex-row justify-between gap-10">
+      <div className="flex flex-col md:flex-row justify-between gap-10">
         <FilterPage></FilterPage>
-        <div className="flex-1"></div>
+        <div className="flex-1">
+          <div className="flex items-center gap-2">
+            <Input
+              type="text"
+              placeholder="Serach by restaurant"
+              value={searchQuery}
+            />
+            <Button className="bg-orange hover:bg-hoverOrange">Search</Button>
+          </div>
+
+          <div>
+            <div className="flex flex-col md:flex-row  md:items-center ">
+              <h1>(2) Result Founds</h1>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
