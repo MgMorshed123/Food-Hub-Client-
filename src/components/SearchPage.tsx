6c3355cc7fad1ca8295824f51d6c2d1ef03cba40
mdarf <mdarfinji45@gmail.com>
2024-11-18 21:42:06 +0800

    added badge

--- src/components/SearchPage.tsx
+++ src/components/SearchPage.tsx
@@ -3,6 +3,8 @@ import FilterPage from "./FilterPage";
 import { Input } from "./ui/input";
 import { useState } from "react";
 import { Button } from "./ui/button";
+import { Badge } from "./ui/badge";
+import { X } from "lucide-react";
 
 const SearchPage = () => {
   const params = useParams();
@@ -23,8 +25,26 @@ const SearchPage = () => {
           </div>
 
           <div>
-            <div className="flex flex-col md:flex-row  md:items-center ">
+            <div className="flex flex-col md:flex-row  md:items-center md:gap-2 my-3">
               <h1>(2) Result Founds</h1>
+
+              <div>
+                {["1", " 2", "3"].map((selected: string, idx: number) => (
+                  <div
+                    className="relative inline-flex items-center max-w-full"
+                    key={idx}
+                  >
+                    <Badge className="text-[#019254]" variant="outline">
+                      {selected}
+                    </Badge>
+                    <X
+                        onClick={() => ""}
+                        size={16}
+                        className="absolute text-[#D19254] right-1 hover:cursor-pointer"
+                      />
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
