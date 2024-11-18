1559063cc70c8259e615a684b5b5127cf9a740e7
mdarf <mdarfinji45@gmail.com>
2024-11-18 22:01:31 +0800

    added search image

--- src/components/SearchPage.tsx
+++ src/components/SearchPage.tsx
@@ -5,6 +5,9 @@ import { useState } from "react";
 import { Button } from "./ui/button";
 import { Badge } from "./ui/badge";
 import { X } from "lucide-react";
+import { Card } from "./ui/card";
+import { AspectRatio } from "./ui/aspect-ratio";
+import HeroImage from "../assets/hero_pizza.png";
 
 const SearchPage = () => {
   const params = useParams();
@@ -28,7 +31,7 @@ const SearchPage = () => {
             <div className="flex flex-col md:flex-row  md:items-center md:gap-2 my-3">
               <h1>(2) Result Founds</h1>
 
-              <div>
+              <div className="flex flex-wrap gap-2 mb-4 md:mb-0">
                 {["1", " 2", "3"].map((selected: string, idx: number) => (
                   <div
                     className="relative inline-flex items-center max-w-full"
@@ -38,13 +41,28 @@ const SearchPage = () => {
                       {selected}
                     </Badge>
                     <X
-                        onClick={() => ""}
-                        size={16}
-                        className="absolute text-[#D19254] right-1 hover:cursor-pointer"
-                      />
+                      onClick={() => ""}
+                      size={16}
+                      className="absolute text-[#D19254] right-1 hover:cursor-pointer"
+                    />
                   </div>
                 ))}
               </div>
+            </div>
+            {/* restaurnat card  */}
+            <div className="grid md:grid-cols-3 gap-4">
+              {/* shadow-xl rounded-xl overflow-hidden hover:shadow-2xl transition-shadow duration-300 */}
+              <Card className="bg-white dark:bg-gray-800  shadow-xl rounded-xl overflow-hidden hover:shadow-2xl transition-shadow duration-300">
+                <div className="relative ">
+                  <AspectRatio ratio={16 / 6}>
+                    <img
+                      src={HeroImage}
+                      className="w-full h-full object-cover"
+                      alt=""
+                    />
+                  </AspectRatio>
+                </div>
+              </Card>
             </div>
           </div>
         </div>
