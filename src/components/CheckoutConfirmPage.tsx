b21cb0277ef39d7948ab333a80e95bdeed1c5b88
mdarf <mdarfinji45@gmail.com>
2024-11-20 23:46:21 +0800

    added check out page

--- src/components/CheckoutConfirmPage.tsx
+++ src/components/CheckoutConfirmPage.tsx
@@ -1,10 +1,15 @@
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import {
   Dialog,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogTitle,
 } from "./ui/dialog";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import { Loader2 } from "lucide-react";
+import { Button } from "./ui/button";
 
 const CheckoutConfirmPage = ({
   open,
@@ -13,20 +18,109 @@ const CheckoutConfirmPage = ({
   open: boolean;
   setOpen: Dispatch<SetStateAction<boolean>>;
 }) => {
+  const checkoutHandler = () => {};
+
+  const changeEventHandler = () => {};
+
+  const loading = true;
+
+  const user = {
+    fullname: "sds",
+    email: "sds",
+    contact: "sds",
+    address: "sds",
+    city: "sds",
+    country: "sds",
+  };
+
+  const [input, setInput] = useState({
+    name: user?.fullname || "",
+    email: user?.email || "",
+    contact: user?.contact.toString() || "",
+    address: user?.address || "",
+    city: user?.city || "",
+    country: user?.country || "",
+  });
+
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent>
-        <DialogTitle>Review Your Order</DialogTitle>
-        <DialogDescription>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
-          obcaecati dolorum aspernatur in. At quasi perferendis reprehenderit,
-          voluptatem quis aliquam?
+        <DialogTitle className="font-semibold">Review Your Order</DialogTitle>
+        <DialogDescription className="text-xs">
+          Double-check your delivery details and ensure everything is in order.
+          When you are ready, hit confirm button to finalize your order
         </DialogDescription>
         <form
           onSubmit={checkoutHandler}
           className="md:grid grid-cols-2 gap-2 space-y-1 md:space-y-0"
         >
-          <div></div>
+          <div>
+            <Label>Fullname</Label>
+            <Input
+              type="text"
+              name="name"
+              value={input.name}
+              onChange={changeEventHandler}
+            />
+          </div>
+          <div>
+            <Label>Email</Label>
+            <Input
+              disabled
+              type="email"
+              name="email"
+              value={input.email}
+              onChange={changeEventHandler}
+            />
+          </div>
+          <div>
+            <Label>Contact</Label>
+            <Input
+              type="text"
+              name="contact"
+              value={input.contact}
+              onChange={changeEventHandler}
+            />
+          </div>
+          <div>
+            <Label>Address</Label>
+            <Input
+              type="text"
+              name="address"
+              value={input.address}
+              onChange={changeEventHandler}
+            />
+          </div>
+          <div>
+            <Label>City</Label>
+            <Input
+              type="text"
+              name="city"
+              value={input.city}
+              onChange={changeEventHandler}
+            />
+          </div>
+          <div>
+            <Label>Country</Label>
+            <Input
+              type="text"
+              name="country"
+              value={input.country}
+              onChange={changeEventHandler}
+            />
+          </div>
+          <DialogFooter className="col-span-2 pt-5">
+            {loading ? (
+              <Button disabled className="bg-orange hover:bg-hoverOrange">
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                Please wait
+              </Button>
+            ) : (
+              <Button className="bg-orange hover:bg-hoverOrange">
+                Continue To Payment
+              </Button>
+            )}
+          </DialogFooter>
         </form>
       </DialogContent>
     </Dialog>
