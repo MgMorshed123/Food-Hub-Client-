11d9376c324e276fefe627a86b08d5c4de9cdd2e
mdarf <mdarfinji45@gmail.com>
2024-11-21 00:42:31 +0800

    added restaurant menu

--- src/admin/Admenu.tsx
+++ src/admin/Admenu.tsx
@@ -158,7 +158,36 @@ const AddMenu = () => {
           </DialogContent>
         </Dialog>
       </div>
-
+      {restaurant?.menus.map((menu: any, idx: number) => (
+        <div key={idx} className="mt-6 space-y-4">
+          <div className="flex flex-col md:flex-row md:items-center md:space-x-4 md:p-4 p-2 shadow-md rounded-lg border">
+            <img
+              src={menu.image}
+              alt=""
+              className="md:h-24 md:w-24 h-16 w-full object-cover rounded-lg"
+            />
+            <div className="flex-1">
+              <h1 className="text-lg font-semibold text-gray-800">
+                {menu.name}
+              </h1>
+              <p className="text-sm tex-gray-600 mt-1">{menu.description}</p>
+              <h2 className="text-md font-semibold mt-2">
+                Price: <span className="text-[#D19254]">80</span>
+              </h2>
+            </div>
+            <Button
+              onClick={() => {
+                setSelectedMenu(menu);
+                setEditOpen(true);
+              }}
+              size={"sm"}
+              className="bg-orange hover:bg-hoverOrange mt-2"
+            >
+              Edit
+            </Button>
+          </div>
+        </div>
+      ))}
       <EditMenu
         selectedMenu={selectedMenu}
         editOpen={editOpen}
