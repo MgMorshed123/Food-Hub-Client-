a00f00bb7395b9af15f5ed42a9cca46ed629b2e6
mdarf <mdarfinji45@gmail.com>
2024-11-26 20:14:01 +0800

    router for admin added

--- src/App.tsx
+++ src/App.tsx
@@ -44,6 +44,18 @@ const AuthenticatedUser = ({ children }: { children: React.ReactNode }) => {
   return children;
 };
 
+const AdminRoute = ({ children }: { children: React.ReactNode }) => {
+  const { user, isAuthenticated } = useUserStore();
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+  if (!user?.admin) {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
+};
+
 const appRouter = createBrowserRouter([
   {
     path: "/",
