78b8ef35c29d749fa6f8add801ca8391f9fafa87
mdarf <mdarfinji45@gmail.com>
2024-11-29 00:30:46 +0800

    set defaulst credentials to true

--- src/store/useRestaurantStore.ts
+++ src/store/useRestaurantStore.ts
@@ -1,9 +1,40 @@
+import Loading from "@/components/Loading";
+import axios from "axios";
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
+import { Toaster} from "sonner";
 
+const API_END_POINT = "http://localhost:4000/api/v1/restaurant";
+
+
+axios.defaults.withCredentials = true;
 export const useRestaurantStore = create()(
   persist(
-    (set) => ({}),
+    (set) => ({
+      loading: false,
+      creatRestaurant: async (formData: FormData) => {
+        try {
+          set({ loading: true });
+          const response = await axios.post(`${API_END_POINT}/signup`, formData, {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+         
+          });
+          if (response.data.success) {
+            Toaster.(response.data.message);
+            set({
+              loading: false,
+              user: response.data.user,
+              isAuthenticated: true,
+            });
+          }
+        } catch (error: any) {
+          toast.error(error.response.data.message);
+          set({ loading: false });
+        }
+      },
+    }),
 
     { name: "restaurant", storage: createJSONStorage(() => localStorage) }
   )
