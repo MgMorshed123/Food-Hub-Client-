4cdd84567fa67245ba343c73c06cd457fc3af19c
mdarf <mdarfinji45@gmail.com>
2024-11-29 00:02:26 +0800

    start

--- src/auth/SignUp.tsx
+++ src/auth/SignUp.tsx
@@ -1,122 +1,119 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Separator } from "@/components/ui/separator";
 import { SignupInputState, userSignupSchema } from "@/schema/userSchema";
 import { useUserStore } from "@/store/useUserStore";
-import { Loader2, LockKeyhole, Mail, SeparatorVertical } from "lucide-react";
+import { Loader2, LockKeyhole, Mail, PhoneOutgoing, User } from "lucide-react";
 import { ChangeEvent, FormEvent, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+// typescript me type define krne ka 2 trika hota hai
 
 const Signup = () => {
   const [input, setInput] = useState<SignupInputState>({
+    fullname: "",
     email: "",
     password: "",
-    fullname: "",
     contact: "",
   });
-
   const [errors, setErrors] = useState<Partial<SignupInputState>>({});
-
+  const { signup, loading } = useUserStore();
+  const navigate = useNavigate();
   const changeEventHandler = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
-
     setInput({ ...input, [name]: value });
   };
-
-  const { signUp, loading } = useUserStore();
-
-  const SignupFormHandler = async (e: FormEvent) => {
+  const loginSubmitHandler = async (e: FormEvent) => {
     e.preventDefault();
-
+    // form validation check start
     const result = userSignupSchema.safeParse(input);
-
     if (!result.success) {
       const fieldErrors = result.error.formErrors.fieldErrors;
       setErrors(fieldErrors as Partial<SignupInputState>);
-
       return;
     }
-
-    await signUp(input);
+    // login api implementation start here
+    try {
+      await signup(input);
+      navigate("/verify-email");
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen ">
+    <div className="flex items-center justify-center min-h-screen">
       <form
-        onSubmit={SignupFormHandler}
-        className="md:p-8 w-full max-w-md md:border  border-gray-200 rounded-lg mx-4"
+        onSubmit={loginSubmitHandler}
+        className="md:p-8 w-full max-w-md rounded-lg md:border border-gray-200 mx-4"
       >
         <div className="mb-4">
-          <h1>Food Hub</h1>
+          <h1 className="font-bold text-2xl">PatelEats</h1>
         </div>
-
         <div className="mb-4">
           <div className="relative">
-            <Label>fullname</Label>
             <Input
-              type="fullname"
+              type="text"
+              placeholder="Full Name"
               name="fullname"
               value={input.fullname}
               onChange={changeEventHandler}
-              placeholder="Enter Your Email "
               className="pl-10 focus-visible:ring-1"
             />
-            <Mail className="absolute inset-y-8 left-2  text-gray-500 pointer-events-none" />
+            <User className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
             {errors && (
               <span className="text-xs text-red-500">{errors.fullname}</span>
             )}
           </div>
         </div>
-
         <div className="mb-4">
           <div className="relative">
-            <Label>Email</Label>
             <Input
               type="email"
+              placeholder="Email"
               name="email"
               value={input.email}
               onChange={changeEventHandler}
-              placeholder="Enter Your Email "
               className="pl-10 focus-visible:ring-1"
             />
-            <Mail className="absolute inset-y-8 left-2  text-gray-500 pointer-events-none" />
+            <Mail className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
             {errors && (
               <span className="text-xs text-red-500">{errors.email}</span>
             )}
           </div>
         </div>
-        <div className="relative">
-          <Label>Password</Label>
-          <Input
-            type="Password"
-            name="password"
-            value={input.password}
-            onChange={changeEventHandler}
-            placeholder="Enter Your Password "
-            className="pl-10 focus-visible:ring-1"
-          />
-          <LockKeyhole className="absolute inset-y-8 left-2 text-gray-500 pointer-events-none" />
-          {errors && (
-            <span className="text-xs text-red-500">{errors.password}</span>
-          )}
+        <div className="mb-4">
+          <div className="relative">
+            <Input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={input.password}
+              onChange={changeEventHandler}
+              className="pl-10 focus-visible:ring-1"
+            />
+            <LockKeyhole className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
+            {errors && (
+              <span className="text-xs text-red-500">{errors.password}</span>
+            )}
+          </div>
         </div>
-
-        <div className="relative">
-          <Label>Contact</Label>
-          <Input
-            type="contact"
-            name="contact"
-            value={input.contact}
-            onChange={changeEventHandler}
-            placeholder="Enter Your contact "
-            className="pl-10 focus-visible:ring-1"
-          />
-          {errors && (
-            <span className="text-xs text-red-500">{errors.contact}</span>
-          )}
+        <div className="mb-4">
+          <div className="relative">
+            <Input
+              type="text"
+              placeholder="Contact"
+              name="contact"
+              value={input.contact}
+              onChange={changeEventHandler}
+              className="pl-10 focus-visible:ring-1"
+            />
+            <PhoneOutgoing className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
+            {errors && (
+              <span className="text-xs text-red-500">{errors.contact}</span>
+            )}
+          </div>
         </div>
-
         <div className="mb-10">
           {loading ? (
             <Button disabled className="w-full bg-orange hover:bg-hoverOrange">
@@ -127,23 +124,15 @@ const Signup = () => {
               type="submit"
               className="w-full bg-orange hover:bg-hoverOrange"
             >
-              SignUp
+              Signup
             </Button>
           )}
-          <div className="mt-4">
-            <Link
-              to="/forgot-password"
-              className="hover:text-blue-500 hover:underline"
-            >
-              Forgot Password
-            </Link>
-          </div>
         </div>
-        <Separator></Separator>
+        <Separator />
         <p className="mt-2">
           Already have an account?{" "}
           <Link to="/login" className="text-blue-500">
-            SignUp
+            Login
           </Link>
         </p>
       </form>
--- src/auth/login.tsx
+++ src/auth/login.tsx
@@ -1,82 +1,82 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Separator } from "@/components/ui/separator";
 import { LoginInputState, userLoginSchema } from "@/schema/userSchema";
 import { useUserStore } from "@/store/useUserStore";
-import { Loader2, LockKeyhole, Mail, SeparatorVertical } from "lucide-react";
+import { Loader2, LockKeyhole, Mail } from "lucide-react";
 import { ChangeEvent, FormEvent, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const { loading, login } = useUserStore();
   const [input, setInput] = useState<LoginInputState>({
     email: "",
     password: "",
   });
   const [errors, setErrors] = useState<Partial<LoginInputState>>({});
+  const { loading, login } = useUserStore();
+  const navigate = useNavigate();
+
   const changeEventHandler = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
-
     setInput({ ...input, [name]: value });
   };
-
-  const LoginFormHandler = async (e: FormEvent) => {
+  const loginSubmitHandler = async (e: FormEvent) => {
     e.preventDefault();
-
     const result = userLoginSchema.safeParse(input);
     if (!result.success) {
       const fieldErrors = result.error.formErrors.fieldErrors;
       setErrors(fieldErrors as Partial<LoginInputState>);
       return;
     }
-
-    await login(input);
+    try {
+      await login(input);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen ">
+    <div className="flex items-center justify-center min-h-screen">
       <form
-        onSubmit={LoginFormHandler}
-        className="md:p-8 w-full max-w-md md:border  border-gray-200 rounded-lg mx-4"
+        onSubmit={loginSubmitHandler}
+        className="md:p-8 w-full max-w-md rounded-lg md:border border-gray-200 mx-4"
       >
         <div className="mb-4">
-          <h1>Food Hub</h1>
+          <h1 className="font-bold text-2xl">PatelEats</h1>
         </div>
-
         <div className="mb-4">
           <div className="relative">
-            <Label>Email</Label>
             <Input
               type="email"
+              placeholder="Email"
               name="email"
               value={input.email}
               onChange={changeEventHandler}
-              placeholder="Enter Your Email "
               className="pl-10 focus-visible:ring-1"
             />
-            <Mail className="absolute inset-y-8 left-2  text-gray-500 pointer-events-none" />
+            <Mail className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
             {errors && (
               <span className="text-xs text-red-500">{errors.email}</span>
             )}
           </div>
         </div>
-        <div className="relative">
-          <Label>Password</Label>
-          <Input
-            type="Password"
-            name="password"
-            value={input.password}
-            onChange={changeEventHandler}
-            placeholder="Enter Your Password "
-            className="pl-10 focus-visible:ring-1"
-          />
-          <LockKeyhole className="absolute inset-y-8 left-2 text-gray-500 pointer-events-none" />
-          {errors && (
-            <span className="text-xs text-red-500">{errors.password}</span>
-          )}
+        <div className="mb-4">
+          <div className="relative">
+            <Input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={input.password}
+              onChange={changeEventHandler}
+              className="pl-10 focus-visible:ring-1"
+            />
+            <LockKeyhole className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
+            {errors && (
+              <span className="text-xs text-red-500">{errors.password}</span>
+            )}
+          </div>
         </div>
-
         <div className="mb-10">
           {loading ? (
             <Button disabled className="w-full bg-orange hover:bg-hoverOrange">
@@ -99,7 +99,7 @@ const Login = () => {
             </Link>
           </div>
         </div>
-        <Separator></Separator>
+        <Separator />
         <p className="mt-2">
           Don't have an account?{" "}
           <Link to="/signup" className="text-blue-500">
